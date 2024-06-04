397ca7ddf95c190d3957f4c0f62cdf89c7501657
Calebe Araujo <calebe.araujo101@gmail.com>
2024-06-04 19:24:56 -0300

    Primeiro comit

---
+++ src/scripts/scripts.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Adicione código JavaScript se necessário para funcionalidades extras,
+// como animações ou interações com o usuário.
+
+// Por exemplo, para ativar a animação de scroll suave para o botão "Agendar Consulta":
+document.querySelector('.btn').addEventListener('click', function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    const targetId = event.target.getAttribute('href'); // Pega o ID do elemento de destino
+    const targetElement = document.querySelector(targetId); // Seleciona o elemento de destino
+    targetElement.scrollIntoView({ behavior: 'smooth' }); // Rola suavemente para o elemento de destino
+});
